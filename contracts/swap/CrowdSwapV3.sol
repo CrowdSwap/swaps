0d22732bf1871195aa9d67ba6125ebf41232ec21
Nader Aryabarzan <aryabarzan@gmail.com>
2024-04-08 17:53:38 +0200

    undo SHB.5 Potential Locking of Funds in The Contract

--- contracts/swap/CrowdSwapV3.sol
+++ contracts/swap/CrowdSwapV3.sol
@@ -96,6 +96,8 @@ contract CrowdSwapV3 is
         uint256 amountOut
     );
 
+    receive() external payable {}
+
     fallback() external {
         revert("CrowdSwapV3: function does  not exist.");
     }
